683e2a37d8210df04a8ff640833e96593e3d5d38
Jon Heywood <94459819+jjheywood@users.noreply.github.com>
2025-01-22 18:25:17 +0100

    feat: added action to create a new key

--- agent-kit-core/typescript/src/actions/warden/index.ts
+++ agent-kit-core/typescript/src/actions/warden/index.ts
@@ -2,7 +2,7 @@ import { WardenAction, WardenActionSchemaAny } from "./warden_action";
 import { GetSpacesAction } from "./get_spaces";
 import { CreateSpaceAction } from "./create_space";
 import { GetKeysAction } from "./get_keys";
-
+import { CreateKeyAction } from "./create_key";
 /**
  * Retrieves all Warden action instances.
  *
@@ -13,6 +13,7 @@ export function getAllWardenActions(): WardenAction<WardenActionSchemaAny>[] {
         new GetSpacesAction(),
         new CreateSpaceAction(),
         new GetKeysAction(),
+        new CreateKeyAction(),
     ];
 }
 
